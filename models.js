56b1f24b513a63694f28568df2aff69944c9dd9b
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-02-25 18:16:39 +0900

    updated userSchema

--- models.js
+++ models.js
@@ -19,11 +19,11 @@ let movieSchema = mongoose.Schema({
 
 //new
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true},
+  birthday: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 userSchema.statics.hashPassword = (password) => {
