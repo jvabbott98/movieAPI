d278e2deeb9683cfce2257531c27d8baffae3170
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-04-30 10:16:32 -0400

    updated mongoose

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-
+// mongoose.connect('mongodb://localhost:27017/moviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
@@ -263,8 +263,3 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
-//new
-
-//old
-//app.listen(8080, () => console.log('Listening on 8080'));
-//old
