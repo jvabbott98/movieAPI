02eaf3a0a148b24e7a6820ffb9005d8689408e62
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-05-09 10:23:18 -0400

    added new origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://justins-myflix-app.netlify.app'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
